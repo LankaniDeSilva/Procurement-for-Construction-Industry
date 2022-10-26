d4031b54e40ddd1850fd4dfa99ec87a286ee9b02
IT20221850 <IT20221850@my.sliit.lk>
2022-10-26 05:50:29 +0530

    Supplier

--- lib/screens/supplier/supplier_home.dart
+++ lib/screens/supplier/supplier_home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:procurement_for_construction_industry/components/custom_text.dart';
+import 'package:procurement_for_construction_industry/controllers/auth_controller.dart';
 
 class SupplierHome extends StatefulWidget {
   const SupplierHome({Key? key}) : super(key: key);
@@ -11,8 +12,19 @@ class SupplierHome extends StatefulWidget {
 class _SupplierHomeState extends State<SupplierHome> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: CustomText(text: 'Supplier Home')),
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            children: [
+              CustomText(text: 'Supplier Home'),
+              InkWell(
+                  onTap: (() => AuthController().signoutUser()),
+                  child: Icon(Icons.logout))
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
